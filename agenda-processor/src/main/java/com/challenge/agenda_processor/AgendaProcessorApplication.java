7047b2188b382a58e06f6538d90daff3fbd00028
Adryen Simoes <islaifer2379@gmail.com>
2025-05-19 23:11:58 -0300

    Add project to consumer open Agenda events

---
+++ agenda-processor/src/main/java/com/challenge/agenda_processor/AgendaProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.agenda_processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaProcessorApplication.class, args);
+	}
+
+}
