11bc7127cace523ed39c1168f87453f8d3b479d0
Adryen Simoes <islaifer2379@gmail.com>
2025-05-19 23:16:13 -0300

    Create final consumer who read Agenda results

---
+++ vote-visualizer/src/main/java/com/challenge/vote_visualizer/VoteVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.vote_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoteVisualizerApplication.class, args);
+	}
+
+}
